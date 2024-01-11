44955129425ba8ebea9248a2128f30a564fbae8b
botii16 <74217394+botii16@users.noreply.github.com>
2024-01-11 07:58:41 +0100

    sdsdf

--- traningapp/Program.cs
+++ traningapp/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace traningapp
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace traningapp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Movement> movements = new List<Movement>();
+
+            try
+            {
+                StreamReader sr = new StreamReader("mozgas.txt");
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split('\t');
+                    if (parts.Length == 3)
+                    {
+                        if (DateTime.TryParse(parts[0], out DateTime date))
+                        {
+                            Movement movement = new Movement(date, parts[1], int.Parse(parts[2]));
+                            movements.Add(movement);
+                        }
+                    }
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading the file: " + e.Message);
+            }
+
+            // Process the data
+            Console.WriteLine("Movements data:");
+            foreach (Movement movement in movements)
+            {
+                Console.WriteLine($"Date: {movement.Date}, Type: {movement.Type}, Duration: {movement.Duration} minutes");
+            }
         }
     }
 }
